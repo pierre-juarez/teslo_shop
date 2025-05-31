8cc13ab17dcac0df51e741e0a3a7a1bee9c6f8f4
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2025-05-30 22:40:26 -0500

    [feat] - Add dotenv and configure environments

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:teslo_shop/config/router/app_router.dart';
-import 'package:teslo_shop/config/theme/app_theme.dart';
+import 'package:teslo_shop/config/config.dart';
 
-void main() {
+void main() async {
+  await Environment.initEnvironment();
+
   runApp(const ProviderScope(child: MainApp()));
 }
 
@@ -12,6 +13,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(Environment.apiUrl);
     return MaterialApp.router(routerConfig: appRouter, theme: AppTheme().getTheme(), debugShowCheckedModeBanner: false);
   }
 }
